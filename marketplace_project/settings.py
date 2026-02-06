0cbc8c513715b127921f07f4d69ba7d876f175f0
40770178-k <153083964+40770178-k@users.noreply.github.com>
2026-02-06 16:27:07 +0300

    Updated settings to inclode user app

--- marketplace_project/settings.py
+++ marketplace_project/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ft022%b(@4+1#1(te673(t@detd*+94y!vin5ex14-2()ij!pv'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'marketplace_app',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
